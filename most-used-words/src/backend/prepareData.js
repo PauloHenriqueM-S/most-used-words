b65da6f645915e4244e9f678e8a219a709a9510f
Paulo Henrique <phttm0501@gmail.com>
2023-10-30 19:28:35 -0400

    end

--- most-used-words/src/backend/prepareData.js
+++ most-used-words/src/backend/prepareData.js
@@ -24,7 +24,7 @@ function filterValid(row) {
 }
 
 function removePonctuation(row) {
-  return row.replace(/[,?!:;.'-]/g, "")
+  return row.replace(/[,?!:;.-]/g, "")
 }
 
 function removeTags(row) {
